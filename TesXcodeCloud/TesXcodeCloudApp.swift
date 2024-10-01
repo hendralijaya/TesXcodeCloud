5ff3574308e50fb3daf35db3fbb98267bb8bbe0d
Hendra Lijaya <hendralijaya@gmail.com>
2024-10-01 11:55:36 +0700

    Initial Commit

---
+++ TesXcodeCloud/TesXcodeCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesXcodeCloudApp.swift
+//  TesXcodeCloud
+//
+//  Created by hendra on 01/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TesXcodeCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TesXcodeCloudTests/TesXcodeCloudTests.swift
@@ -0,0 +1,17 @@
+//
+//  TesXcodeCloudTests.swift
+//  TesXcodeCloudTests
+//
+//  Created by hendra on 01/10/24.
+//
+
+import Testing
+@testable import TesXcodeCloud
+
+struct TesXcodeCloudTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
